8ee27b0e060e03b6efdcc6782d4ac6e281802361
Hnatiak <romanhnatiak@gmail.com>
2023-04-26 13:32:07 +0200

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,8 @@
-// import { Component, Suspense, lazy } from "react";
-// import { Routes, Route } from "react-router-dom";
-// import { connect } from "react-redux";
-// import AppBar from "./components/AppBar";
-// import authOperations from "./redux/app/app-operations";
-// import PrivateRoute from "./components/PrivateRoute";
-// import PublicRoute from "./components/PublicRoute";
-
-// // const Layout = lazy(() => import("./views/Layout"));
-// const HomePage = lazy(() => import("./views/HomeViews"));
-// const LoginPage = lazy(() => import("./views/LoginView"));
-// const RegisterPage = lazy(() => import("./views/RegisterViews"));
-// const UsersPage = lazy(() => import("./views/ContactsView"));
-
-// class App extends Component {
-//   // componentDidMount() {
-//   //   this.props.onGetCurrentUser();
-//   // }
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <AppBar />
-//         <Suspense fallback={<p>Loading...</p>}>
-//             <Routes>
-//               <Route path="/" exact='true' element={<HomePage />}>
-//                 <Route
-//                   index
-//                   element={
-//                     <PublicRoute>
-//                       <HomePage />
-//                     </PublicRoute>
-//                   }
-//                 />
-//                 <Route
-//                   path="/contacts"
-//                   element={
-//                     <PrivateRoute>
-//                       <UsersPage path="/contacts" component={UsersPage}/>
-//                     </PrivateRoute>
-//                   }
-//                 />
-//                 <Route
-//                   path="register"
-//                   redirectTo="/contacts"
-//                   element={
-//                     <PublicRoute restricted>
-//                       <RegisterPage path="/register" component={RegisterPage}/>
-//                     </PublicRoute>
-//                   }
-//                 />
-//                 <Route
-//                   path="login"
-//                   redirectTo="/contacts"
-//                   element={
-//                     <PublicRoute restricted>
-//                       <LoginPage path="/login" component={LoginPage}/>
-//                     </PublicRoute>
-//                   }
-//                 />
-//               </Route>
-//             </Routes>
-//         </Suspense>
-//       </div>
-//     );
-//   }
-// }
-
-// const mapDispatchToProrps = {
-//   onGetCurrentUser: authOperations.getCurrentUser,
-// };
-
-// export default connect(null, mapDispatchToProrps)(App);
-
-
-
-
-
-
-
-
-
 import { Component, Suspense, lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { connect } from "react-redux";
 import AppBar from "./components/AppBar";
-import authOperations from "./redux/app/app-operations";
+import authOperations from "./redux/auth/auth-operations";
 import PrivateRoute from "./components/PrivateRoute";
 import PublicRoute from "./components/PublicRoute";
 
@@ -94,9 +12,9 @@ const RegisterPage = lazy(() => import("./views/RegisterViews"));
 const UsersPage = lazy(() => import("./views/ContactsView"));
 
 class App extends Component {
-  // componentDidMount() {
-  //   this.props.onGetCurrentUser();
-  // }
+  componentDidMount() {
+    this.props.onGetCurrentUser();
+  }
 
   render() {
     const { isAuthenticated } = this.props;
@@ -167,3 +85,99 @@ const mapDispatchToProps = {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Suspense, lazy, useEffect } from "react";
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import { connect } from "react-redux";
+// import AppBar from "./components/AppBar";
+// import authOperations from "./redux/auth/auth-operations";
+// import PrivateRoute from "./components/PrivateRoute";
+// import PublicRoute from "./components/PublicRoute";
+
+// const HomePage = lazy(() => import("./views/HomeViews"));
+// const LoginPage = lazy(() => import("./views/LoginView"));
+// const RegisterPage = lazy(() => import("./views/RegisterViews"));
+// const UsersPage = lazy(() => import("./views/ContactsView"));
+
+// function App({ isAuthenticated, getCurrentUser }) {
+//   useEffect(() => {
+//     getCurrentUser();
+//   }, [getCurrentUser]);
+
+//   return (
+//     <div className="App">
+//       <AppBar />
+//       <Suspense fallback={<p>Loading...</p>}>
+//         <Routes>
+//           <Route path="/" element={<HomePage />}>
+//             <Route
+//               index
+//               element={
+//                 <PublicRoute>
+//                   <HomePage />
+//                 </PublicRoute>
+//               }
+//             />
+//             <Route
+//               path="contacts"
+//               element={
+//                 isAuthenticated ? (
+//                   <PrivateRoute>
+//                     <UsersPage />
+//                   </PrivateRoute>
+//                 ) : (
+//                   <Navigate to="/login" replace />
+//                 )
+//               }
+//             />
+//             <Route
+//               path="register"
+//               element={
+//                 isAuthenticated ? (
+//                   <Navigate to="/contacts" replace />
+//                 ) : (
+//                   <PublicRoute restricted>
+//                     <RegisterPage />
+//                   </PublicRoute>
+//                 )
+//               }
+//             />
+//             <Route
+//               path="login"
+//               element={
+//                 isAuthenticated ? (
+//                   <Navigate to="/contacts" replace />
+//                 ) : (
+//                   <PublicRoute restricted>
+//                     <LoginPage />
+//                   </PublicRoute>
+//                 )
+//               }
+//             />
+//           </Route>
+//         </Routes>
+//       </Suspense>
+//     </div>
+//   );
+// }
+
+// const mapStateToProps = (state) => ({
+//   isAuthenticated: state.auth.isAuthenticated,
+// });
+
+// const mapDispatchToProps = {
+//   getCurrentUser: authOperations.getCurrentUser,
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -96,11 +96,106 @@
 
 
 
+// import React, { Component } from "react";
+// import { connect } from "react-redux";
+// import authOperations from "../redux/auth/auth-operations";
+// import {Button} from "@mui/material";
+// import {TextField} from "@mui/material";
+
+// const styles = {
+//   form: {
+//     width: 320,
+//   },
+//   label: {
+//     display: "flex",
+//     flexDirection: "column",
+//     marginBottom: 15,
+//   },
+// };
+
+// class LoginView extends Component {
+//   state = {
+//     email: "",
+//     password: "",
+//   };
+
+//   handleChange = ({ target: { name, value } }) => {
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     this.props.onLogin(this.state);
+
+//     this.setState({ name: "", email: "", password: "" });
+//   };
+
+//   render() {
+//     const { email, password } = this.state;
+
+//     return (
+//       <div style={{ marginTop: "35px" }}>
+//         <h1 style={{ marginBottom: "35px" }}>Login</h1>
+
+//         <form
+//           onSubmit={this.handleSubmit}
+//           style={styles.form}
+//           autoComplete="off"
+//         >
+//           <TextField
+//             style={{ marginBottom: "15px", width: 300}}
+//             label="E-mail:"
+//             type="email"
+//             name="email"
+//             value={email}
+//             onChange={this.handleChange}
+//           />
+//           <TextField
+//             style={{ width: 300}}
+//             label="Password:"
+//             type="password"
+//             name="password"
+//             value={password}
+//             onChange={this.handleChange}
+//           />
+//           <br />
+//           <Button
+//             style={{ marginTop: 25 }}
+//             type="submit"
+//             variant="contained"
+//             color="primary"
+//           >
+//             SIGN IN ✅
+//           </Button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapDispatchToProps = {
+//   onLogin: authOperations.logIn,
+// };
+// //Full version
+// /* const mapDispatchToProps = dispatch=>{
+//   onSubmit: (data)=>dispatch (authOperations.register(data))
+// } */
+
+// export default connect(null, mapDispatchToProps)(LoginView);
+
+
+
+
+
+
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import authOperations from "../redux/auth/auth-operations";
-import {Button} from "@mui/material";
-import {TextField} from "@mui/material";
+import { Button } from "@mui/material";
+import { TextField } from "@mui/material";
+import { IconButton, InputAdornment } from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 const styles = {
   form: {
@@ -117,10 +212,17 @@ class LoginView extends Component {
   state = {
     email: "",
     password: "",
+    showPassword: true,
   };
 
   handleChange = ({ target: { name, value } }) => {
     this.setState({ [name]: value });
+  };
+
+  togglePasswordVisibility = () => {
+    this.setState((prevState) => ({
+      showPassword: !prevState.showPassword,
+    }));
   };
 
   handleSubmit = (e) => {
@@ -132,7 +234,7 @@ class LoginView extends Component {
   };
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, showPassword } = this.state;
 
     return (
       <div style={{ marginTop: "35px" }}>
@@ -144,7 +246,7 @@ class LoginView extends Component {
           autoComplete="off"
         >
           <TextField
-            style={{ marginBottom: "15px", width: 300}}
+            style={{ marginBottom: "15px", width: 300 }}
             label="E-mail:"
             type="email"
             name="email"
@@ -152,12 +254,25 @@ class LoginView extends Component {
             onChange={this.handleChange}
           />
           <TextField
-            style={{ width: 300}}
+            style={{ width: 300 }}
             label="Password:"
-            type="password"
+            type={showPassword ? "password" : "text"}
             name="password"
             value={password}
             onChange={this.handleChange}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={this.togglePasswordVisibility}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
           <br />
           <Button
@@ -177,10 +292,6 @@ class LoginView extends Component {
 const mapDispatchToProps = {
   onLogin: authOperations.logIn,
 };
-//Full version
-/* const mapDispatchToProps = dispatch=>{
-  onSubmit: (data)=>dispatch (authOperations.register(data))
-} */
 
 export default connect(null, mapDispatchToProps)(LoginView);
 
